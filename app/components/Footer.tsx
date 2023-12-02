cd75399c84d20220b2d3dbcc96474918fd44eeb2
yoGhastly <ve06976@innovaccion.mx>
2023-12-02 14:52:30 -0600

    whatsapp contact on footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,24 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-nowrap md:flex-wrap h-24 w-full items-center justify-center">
-      <p className="text-white text-center">Made with ❤️ by Xervsware</p>
+    <footer className="flex h-24 w-full items-center justify-center">
+      <Link
+        href="https://wa.me/message/JXBY6AHVQ5VSO1"
+        target="_blank"
+        className="font-bold flex gap-2 justify-center"
+      >
+        <p>Contact us on WhatsApp</p>
+        <Image
+          src="/assets/images/WhatsApp.svg"
+          alt="Xervsware WhatsApp"
+          width={24}
+          height={24}
+        />
+      </Link>
     </footer>
   );
 };
